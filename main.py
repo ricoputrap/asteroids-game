931bd6797af8b1032055d589dd82808804236196
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2026-05-06 21:35:44 +0700

    Try import pygame and print the version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from asteroids-game!")
+import pygame
 
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 
 if __name__ == "__main__":
     main()
