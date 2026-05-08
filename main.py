f87d489e2c4ed205e13688c65eca9cddab282a3a
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2026-05-08 23:42:02 +0700

    implement collision between player and any asteroid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -37,6 +38,14 @@ def main():
 
         screen.fill("black")
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            is_colliding = asteroid.collides_with(player)
+
+            if is_colliding:
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         for drawable_item in drawable:
             drawable_item.draw(screen)
